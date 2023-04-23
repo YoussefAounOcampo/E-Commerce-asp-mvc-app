bace6be4e19558c0d135b614f3232cf9f8f0ccff
YOUSSEF AOUN OCAMPO <guryxxx@hotmail.com>
2023-04-23 11:42:36 +0200

    Añado en Models Actor, Cinema, Movie, Producer. Creo la carpeta Data y dentro un Enum de categorias de peliculas.

---
+++ E_Commerce_Peliculas/Data/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce_Peliculas.Data
+{
+    public enum MovieCategory
+    {
+        Accion=1,
+        Comedia,
+        Drama,
+        Documentary
+    }
+}
---
+++ E_Commerce_Peliculas/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace E_Commerce_Peliculas.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+    }
+}
